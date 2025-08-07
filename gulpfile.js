20cbf399933acb2e13b191df234e0136b934a16c
Zenith-One <the.number.zero@gmail.com>
2025-08-07 14:27:00 -0400

    Documentation infrastructure setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const { spawn } = require('child_process');
 
 /* ----------------------------------------- */
 /*  Compile sass
@@ -11,12 +12,41 @@ function compileSass() {
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./styles'));
 };
+
+const DOCS_SASS = ["documentation/sass/**/*.scss"]
+function compileDocSass() {
+  return gulp.src('./documentation/sass/custom.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./documentation/docs/overrides/assets/stylesheets/'));
+};
 /* ----------------------------------------- */
 /*  Watch Updates
 /* ----------------------------------------- */
 
 function watchUpdates() {
   gulp.watch(SIMPLE_SASS, compileSass);
+}
+
+function watchDocs(){
+  gulp.watch(DOCS_SASS, compileDocSass);
+}
+
+function mkdocs() {
+  try {
+    console.log('Starting mkdocs');
+    const mk = spawn('mkdocs', ['serve']);
+
+    // Handle output for mkdocs
+    mk.stdout.on('data', (data) => {
+      console.log(`[MkDocs] : ${data}`);
+    });
+    mk.stderr.on('data', (data) => {
+      console.error(`[MkDocs] : ${data}`);
+    });
+
+  } catch (e) {
+
+  }
 }
 
 /* ----------------------------------------- */
@@ -28,5 +58,12 @@ module.exports = {
     gulp.parallel(compileSass),
     watchUpdates
   ),
-  css: compileSass
+  css: compileSass,
+  docs: gulp.parallel(
+    gulp.series(
+      gulp.parallel(compileDocSass),
+      watchDocs
+    ),
+    mkdocs
+  )
 }
